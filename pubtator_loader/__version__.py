c2e9cfc6633c57a5b7893f994a1e985e8f5f7b93
ArshSekhon <arshs3khon@gmail.com>
2021-01-29 23:49:11 -0800

    Changed version info

--- pubtator_loader/__version__.py
+++ pubtator_loader/__version__.py
@@ -5,7 +5,7 @@ within the package:
 Example:
     >>> from pubtator_loader.__version__ import __version__
 """
-__name__ = "PubTatorLoader"
+__name__ = "pubtator_loader"
 __version__ = "0.1.0"
 __description__ = "Parse PubTator files with ease"
 __url__ = "https://github.com/arshsekhon/pubtator_loader"
